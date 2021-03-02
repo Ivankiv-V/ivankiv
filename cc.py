549b88a37744da9017f331287958169e40a11231
Ivankiv-V <79783523+Ivankiv-V@users.noreply.github.com>
2021-03-02 23:30:03 +0300

    Add files via upload

---
+++ cc.py
@@ -0,0 +1,24 @@
+str_input = input("Delimoe: ")
+delimoe = int(str_input)
+
+operation = input ("+ / * - ^ ")
+str_input2 = input("Delitel: ")
+delitel = int(str_input2)
+
+if operation == "+":
+	result = delimoe + delitel
+elif operation == "/":
+         if delitel != 0:
+                 result = delimoe / delitel
+         else:
+               result = "Inf"          
+elif operation == "*":
+        result = delimoe * delitel
+elif operation == "-":
+	 result = delimoe - delitel
+elif operation == "^":
+        result = delimoe ^ delitel
+
+	
+print("Result: " + str(result))
+
